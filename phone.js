979858d57d41704edeceb1bd0ae8e095e909c29a
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-08-28 11:15:36 +0600

    added lodging

--- phone.js
+++ phone.js
@@ -1,6 +1,6 @@
-const loadPhone = async () => {
+const loadPhone = async (searchFiled) => {
   const res = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
+    `https://openapi.programming-hero.com/api/phones?search=${searchFiled}`
   );
   const data = await res.json();
   const phones = data.data;
@@ -8,22 +8,56 @@ const loadPhone = async () => {
   displayPhones(phones);
 };
 const displayPhones = (phones) => {
+  const cardContainer = document.getElementById("card-container");
+  // clear card container container before add new card
+  cardContainer.textContent = "";
+  const loadAllButton = document.getElementById('load-all-button')
+  if(phones.length > 12){
+      loadAllButton.classList.remove('hidden')
+    }else{
+        loadAllButton.classList.add('hidden')
+    }
+    // display only first 12 phone
+    phones = phones.slice(0, 12);
+//   const showAllCard = () => {
+
+//   }
   phones.forEach((phone) => {
-    const cardContainer = document.getElementById("card-container");
     const card = document.createElement("div");
-    card.classList = `card m-10 bg-gray-100 shadow-xl`;
+    card.classList = `card p-6 bg-base-100 shadow-xl`;
     card.innerHTML = `
-            <figure><img src="${phone.image}" alt="Shoes" /></figure>
+            <figure>
+                 <img src="${phone.image}" alt="Shoes" />
+            </figure>
             <div class="card-body">
-                <h2 class="card-title">Shoes!</h2>
+                <h2 class="card-title">${phone.phone_name}</h2>
                 <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+                <div class="card-actions justify-center ">
+                    <button class="btn w-3/6 btn-primary mt-5">Buy Now</button>
                 </div>
             </div>
         `;
     cardContainer.appendChild(card);
-    console.log(phone.image);
+    // console.log(phone);
   });
+  toggleLodgingSpinner(false)
+
 };
-loadPhone();
+
+// document.getElementById('search-btn').addEventListener('click', function())
+const handelSearch = () => {
+    toggleLodgingSpinner(true)
+  const searchInput = document.getElementById("search-input");
+  const searchText = searchInput.value;
+  fetch("https://openapi.programming-hero.com/api/phones?search=${searchText}");
+  loadPhone(searchText);
+};
+
+const toggleLodgingSpinner = (isLoading) =>{
+    const loadingSpinner = document.getElementById('loading-spinner');
+    if(isLoading){
+    loadingSpinner.classList.remove('hidden')
+    }else{
+    loadingSpinner.classList.add('hidden')
+    }
+}
