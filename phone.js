c31f94ef3cecb14bcebf8104efd32b6a812013e6
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-08-28 19:53:16 +0600

    show phone details

--- phone.js
+++ phone.js
@@ -8,23 +8,23 @@ const loadPhone = async (searchFiled, isAllShow) => {
   displayPhones(phones, isAllShow);
 };
 const displayPhones = (phones, isAllShow) => {
-  console.log(phones)
+  // console.log(phones);
   const cardContainer = document.getElementById("card-container");
   // clear card container container before add new card
   cardContainer.textContent = "";
-  const loadAllButton = document.getElementById('load-all-button')
-  if(phones.length > 12 && !isAllShow){
-      loadAllButton.classList.remove('hidden')
-    }else{
-        loadAllButton.classList.add('hidden')
-    }
-    // display only first 12 phone
-    if(!isAllShow){
-
-      phones = phones.slice(0, 12);
-    }
+  const loadAllButton = document.getElementById("load-all-button");
+  if (phones.length > 12 && !isAllShow) {
+    loadAllButton.classList.remove("hidden");
+  } else {
+    loadAllButton.classList.add("hidden");
+  }
+  // display only first 12 phone
+  if (!isAllShow) {
+    phones = phones.slice(0, 12);
+  }
 
   phones.forEach((phone) => {
+    console.log(phone)
     const card = document.createElement("div");
     card.classList = `card p-6 bg-base-100 shadow-xl`;
     card.innerHTML = `
@@ -42,39 +42,66 @@ const displayPhones = (phones, isAllShow) => {
     cardContainer.appendChild(card);
     // console.log(phone);
   });
-  toggleLodgingSpinner(false)
+  toggleLodgingSpinner(false);
+};
 
+// show modal
+const showMyModal = async (id) => {
+  // load individual data
+  console.log("modal", id);
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+  const data = await res.json();
+  const phone = data.data;
+  showModalDetails(phone);
+};
+
+// show modal details
+const showModalDetails = (phone) => {
+  // const phoneName = document.getElementById('phone-name')
+  // phoneName.innerText= phone.name;
+  const showModalContainer = document.getElementById('show-modal-container');
+  showModalContainer.innerHTML= `
+  <div class="bg-sky-100">
+      <img src='${phone.image}' class="w-32 mx-auto py-6 rounded-xl mb-6 h-52 ">
+  </div>
+  <h3 id="phone-name" class="font-bold text-2xl">${phone.name}</h3>
+  <p><span class="font-semibold text-xl">Stores:</span> ${phone?.mainFeatures?.storage}</p>
+  <p><span class="font-semibold text-lg">Display Size :</span> ${phone?.mainFeatures?.displaySize}</p>
+  <p><span class="font-semibold text-lg">Chipset :</span> ${phone?.mainFeatures?.chipSet}</p>
+  <p><span class="font-semibold text-lg">Memory :</span> ${phone?.mainFeatures?.memory}</p>
+  <p><span class="font-semibold text-lg">Slug :</span> ${phone?.slug}</p>
+  <p><span class="font-semibold text-lg">Release data :</span> ${phone?.releaseDate || 'no bailable'}</p>
+  <p><span class="font-semibold text-lg">Brand :</span> ${phone?.brand}</p>
+  <p><span class="font-semibold text-lg">GPS :</span> ${phone?.others?.GPS}</p>
+  `
+  console.log(phone)
+  show_details_modal.showModal();
 };
 
 // document.getElementById('search-btn').addEventListener('click', function())
 const handelSearch = (siAllShow) => {
-    toggleLodgingSpinner(true)
+  toggleLodgingSpinner(true);
   const searchInput = document.getElementById("search-input");
   const searchText = searchInput.value;
   fetch("https://openapi.programming-hero.com/api/phones?search=${searchText}");
   loadPhone(searchText, siAllShow);
 };
 
-const toggleLodgingSpinner = (isLoading) =>{
-    const loadingSpinner = document.getElementById('loading-spinner');
-    if(isLoading){
-    loadingSpinner.classList.remove('hidden')
-    }else{
-    loadingSpinner.classList.add('hidden')
-    }
-}
+const toggleLodgingSpinner = (isLoading) => {
+  const loadingSpinner = document.getElementById("loading-spinner");
+  if (isLoading) {
+    loadingSpinner.classList.remove("hidden");
+  } else {
+    loadingSpinner.classList.add("hidden");
+  }
+};
 
 // load all
-const loadAllCard = () =>{
-    handelSearch(true)
-}
+const loadAllCard = () => {
+  handelSearch(true);
+};
 
-// show modal
-const showMyModal = async (id) =>{
-  console.log('modal', id)
-  // load individual data
-  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
-  const data = res.json();
-  console.log(data)
-  document.getElementById('my_modal_5', )
-}
+
+
+
+
